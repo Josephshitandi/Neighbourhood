4c9c7d5b025af030e7bf07aa6bffe146c9bf97f6
rowenarono95 <rowenarono@gmail.com>
2020-12-01 21:27:10 +0300

    create models

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
+ 
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+
+ 
+class Neighbourhood(models.Model):
+    neighbourhoodName = models.CharField(max_length=250)
+    neighbourhoodLocation = models.CharField(max_length=250)
+    occupantsCount = models.IntegerField(default=0)
+    admin = models.ForeignKey(User,on_delete=models.CASCADE)
