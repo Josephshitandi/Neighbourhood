5c7cb8fca9b5c2851487fa27877de67be66b2442
Josephshitandi <jsphshtnd@gmail.com>
2020-12-02 14:54:38 +0300

    Add profile model

--- hood/models.py
+++ hood/models.py
@@ -6,9 +6,26 @@ class Neighbourhood(models.Model):
     hoodLocation = models.CharField(max_length=250)
     occupantsCount = models.IntegerField(default=0)
     admin = models.ForeignKey(User,on_delete=models.CASCADE)
+    
     def __str__(self):
         return f'{self.hoodName} neighbourhood'
+    
     def save_neighborhood(self):
         self.save()
     def delete_neighborhood(self):
         self.delete()
+        
+class Profile (models.Model):
+    name = models.CharField(max_length=30)
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    email = models.CharField(max_length=50)
+    status = models.BooleanField()
+    image = models.ImageField(upload_to = 'profile',default = 'profile.jpg')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    def save_profile(self):
+        self.save
+    def delete_profile(self):
+        self.delete()
+        
