43227fcce8e6230f3b10a201bc24c3dc581badf7
Josephshitandi <jsphshtnd@gmail.com>
2020-12-04 20:21:03 +0300

    Modifies project settings

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -57,6 +57,14 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'hood.User'
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:8000",
+    "http://localhost:4200",
+)
+
+
 CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
